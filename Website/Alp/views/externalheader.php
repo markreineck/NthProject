e94742aa86d25081a24442bd75cd510b6273d522
Mark Reineck <mark@nth-generation.com>
2015-03-24 18:10:45 -0400

    no message

--- Website/Alp/views/externalheader.php
+++ Website/Alp/views/externalheader.php
@@ -2,5 +2,9 @@
 	<p align="left" valign="bottom" style="color: #989898; font-size:18px; padding:0; font-weight:900; text-shadow: 0px 1px 0px #000;">
 <?php echo $this->UserSetting('SubscriptionName'); ?>
 	</p>
-	<p>Project Management Portal</p>
+	<p>Project Management Portal
+<div style="float: right;">
+<?php echo $_SERVER['REMOTE_ADDR']; ?>
+</div>
+	</p>
 </div>
