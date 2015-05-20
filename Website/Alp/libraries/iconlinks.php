86bd6a7ee473dfe09110c45e541816434c15d27f
Mark Reineck <mark@nth-generation.com>
2015-05-20 16:08:01 -0400

    Update iconlinks.php

--- Website/Alp/libraries/iconlinks.php
+++ Website/Alp/libraries/iconlinks.php
@@ -4,7 +4,8 @@ function MakeIconLink($icon, $link, $hint, $onclick='', $color='')
 	echo "<a href=\"$link\" onclick=\"$onclick\" class=\"ActionIconLinks $color\"><span class=\"glyphicon glyphicon-$icon\"></span></a>";
 }
 
-function MakeJSIcon($icon, $js, $hint, $size)
+// $size is currently not used. Why did we add this?
+function MakeJSIcon($icon, $js, $hint, $size='')
 {
 	//echo "<img src=\"/image/$icon\" alt=\"$hint\" title=\"$hint\" class=\"icons\" border=\"0\" onClick=\"$js\">";
 	echo "<a href=\"#hint\" onclick=\"$js\" alt=\"$hint\" class=\"ActionIconLinks\"><span class=\"glyphicon glyphicon-$icon\"></span></a>";
