9023bc7853635f83541a255fa5d5dbc4c4432b90
Mark Reineck <mark@nth-generation.com>
2015-06-04 14:47:38 -0400

    new template

--- Website/Alp/controllers/userstatuses.php
+++ Website/Alp/controllers/userstatuses.php
@@ -1,16 +1,11 @@
 <?php
 class userstatuses extends AlpFramework implements AlpController {
 
-public function __construct($url)
-{
-	parent::AlpFramework($url);
-}
-
 function Start()
 {
 	$c = $this->Cookie('ProjectCookie');
 	$db = $this->LoadModel(array('DatabaseDB', 'SuperDB'));
-	$this->LoadView('home');
+	$this->LoadView('template2015');
 }
 }
 ?>
