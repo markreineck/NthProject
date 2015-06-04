86761fea2813107a846494c5ece39eb1086d9125
Mark Reineck <mark@nth-generation.com>
2015-06-04 15:18:27 -0400

    Update companies.php

--- Website/Alp/controllers/companies.php
+++ Website/Alp/controllers/companies.php
@@ -1,15 +1,7 @@
 <?php
 include 'OrgController.php';
 
-class companies extends AlpFramework implements AlpController {
-
-public function __construct($url)
-{
-	parent::AlpFramework($url);
-	$c = $this->Cookie('ProjectCookie');
-	$db = $this->LoadModel(array('DatabaseDB', 'OrgDB'));
-	$db->ValidateUserSession($this->Cookie());
-}
+class companies extends OrgController implements AlpController {
 
 function Start()
 {
