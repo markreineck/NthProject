6821f9441e6db78723a5677f16b356c9e3529a4b
Rey Suralta <j19ox85@yahoo.com>
2015-09-03 06:52:50 +0800

    Menu Changes

--- Website/Alp/views/menu2015.php
+++ Website/Alp/views/menu2015.php
@@ -1,77 +1,99 @@
 <?php
-$timemenu = array(
-	array('priv'=>'',	'opt'=>'',				'link'=>'signin',			'name'=>'Project Sign In'),
-	array('priv'=>'',	'opt'=>'',				'link'=>'signout',			'name'=>'Signout'),
-	array('priv'=>'G',	'opt'=>'',				'link'=>'recordtime',		'name'=>'Record Time'),
-	array('priv'=>'',	'opt'=>'',				'link'=>'mytime',			'name'=>'My Time'),
-	array('priv'=>'',	'opt'=>'',				'link'=>'signedin',			'name'=>'Signed-in Staff')
-);
 
-$timerptmenu = array(
-	array('priv'=>'G',	'opt'=>'Time',			'link'=>'timedetail',		'name'=>'Time Detail'),
-	array('priv'=>'G',	'opt'=>'Time',			'link'=>'payroll',			'name'=>'Payroll'),
-	array('priv'=>'G',	'opt'=>'Time',			'link'=>'timeexport',		'name'=>'Time Log'),
-	array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'paytasks',			'name'=>'Pay For Tasks'),
-	array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'payhistory',		'name'=>'Payment History'),
-	array('priv'=>'',	'opt'=>'TaskCost',		'link'=>'mypayhistory',		'name'=>'My Payment History'),
-	array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'billtask',			'name'=>'Bill Tasks'),
-	array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'billhistory',		'name'=>'Billing History'),
-	array('priv'=>'G',	'opt'=>'',				'link'=>'projectcost',		'name'=>'Project Costs')
-);
-
-$taskmenu = array(
-	array('priv'=>'',	'opt'=>'',				'link'=>'taskbypriority',	'name'=>'Active Tasks'),
-	array('priv'=>'',	'opt'=>'TaskCost',		'link'=>'taskcosts',		'name'=>'Fixed Price Tasks'),
-	array('priv'=>'',	'opt'=>'Milestones',	'link'=>'taskbymilestone',	'name'=>'Tasks by Milestone'),
-	array('priv'=>'',	'opt'=>'',				'link'=>'tasksremoved',		'name'=>'Cancelled Tasks')
-);
-
-$taskrptmenu = array(
-	array('priv'=>'',	'opt'=>'',				'link'=>'taskcompleted',	'name'=>'Completed'),
-	array('priv'=>'',	'opt'=>'',				'link'=>'taskrecentdone',	'name'=>'Recently Completed '),
-	array('priv'=>'',	'opt'=>'ApproveTasks',	'link'=>'taskapproved',		'name'=>'Approved'),
-	array('priv'=>'',	'opt'=>'ApproveTasks',	'link'=>'taskrecentappr',	'name'=>'Recently Approved '),
-	array('priv'=>'',	'opt'=>'ReleaseTasks',	'link'=>'releasetasks',		'name'=>'Release Tasks'),
-	array('priv'=>'',	'opt'=>'ReleaseTasks',	'link'=>'taskrecentrel',	'name'=>'Recently Released ')
-);
-
-$projectmenu = array(
-	array('priv'=>'',	'opt'=>'',				'link'=>'projects',			'name'=>'Projects'),
-	array('priv'=>'',	'opt'=>'',				'link'=>'projectareas',		'name'=>'Project Areas'),
-	array('priv'=>'',	'opt'=>'Milestones',	'link'=>'milestones',		'name'=>'Milestones'),
-	array('priv'=>'S',	'opt'=>'',				'link'=>'projectdefaults',	'name'=>'Project Defaults'),
-	array('priv'=>'S',	'opt'=>'',				'link'=>'projectadd',		'name'=>'Add Project')
-);
-
-$supermenu = array(
-	array('priv'=>'G',	'opt'=>'Organizations',	'link'=>'companies',		'name'=>'Organizations'),
-	array('priv'=>'U',	'opt'=>'',				'link'=>'users',			'name'=>'Users'),
-	array('priv'=>'U',	'opt'=>'',				'link'=>'useradd',			'name'=>'Create User'),
-	array('priv'=>'G',	'opt'=>'Organizations',	'link'=>'orgtypes ',		'name'=>'Organizaton Types'),
-	array('priv'=>'G',	'opt'=>'',				'link'=>'userstatuses ',	'name'=>'User Status'),
-	array('priv'=>'G',	'opt'=>'',				'link'=>'taskstatuses ',	'name'=>'Task Status'),
-	array('priv'=>'G',	'opt'=>'',				'link'=>'purgetime ',		'name'=>'Purge Time'),
-	array('priv'=>'G',	'opt'=>'',				'link'=>'purgetask ',		'name'=>'Purge Task')
-/*
-,
-	array('priv'=>'G',	'link'=>'settings',			'name'=>'Settings')
-*/
-);
 
 function ShowMenu($alp, $menuname, $menuid)
 {
 	$db = $alp->Model();
+		
+	$timemenu = array(
+		array('priv'=>'',	'opt'=>'',				'link'=>'signin',			'name'=>'Project Sign In'),
+		array('priv'=>'',	'opt'=>'',				'link'=>'signout',			'name'=>'Signout'),
+		array('priv'=>'G',	'opt'=>'',				'link'=>'recordtime',		'name'=>'Record Time'),
+		array('priv'=>'',	'opt'=>'',				'link'=>'mytime',			'name'=>'My Time'),
+		array('priv'=>'',	'opt'=>'',				'link'=>'signedin',			'name'=>'Signed-in Staff')
+	);
+	
+	$timerptmenu = array(
+		array('priv'=>'G',	'opt'=>'Time',			'link'=>'timedetail',		'name'=>'Time Detail'),
+		array('priv'=>'G',	'opt'=>'Time',			'link'=>'payroll',			'name'=>'Payroll'),
+		array('priv'=>'G',	'opt'=>'Time',			'link'=>'timeexport',		'name'=>'Time Log'),
+		array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'paytasks',			'name'=>'Pay For Tasks'),
+		array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'payhistory',		'name'=>'Payment History'),
+		array('priv'=>'',	'opt'=>'TaskCost',		'link'=>'mypayhistory',		'name'=>'My Payment History'),
+		array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'billtask',			'name'=>'Bill Tasks'),
+		array('priv'=>'G',	'opt'=>'TaskCost',		'link'=>'billhistory',		'name'=>'Billing History'),
+		array('priv'=>'G',	'opt'=>'',				'link'=>'projectcost',		'name'=>'Project Costs')
+	);
+	
+	$taskmenu = array(
+		array('priv'=>'',	'opt'=>'',				'link'=>'taskbypriority',	'name'=>'Active Tasks'),
+		array('priv'=>'',	'opt'=>'TaskCost',		'link'=>'taskcosts',		'name'=>'Fixed Price Tasks'),
+		array('priv'=>'',	'opt'=>'Milestones',	'link'=>'taskbymilestone',	'name'=>'Tasks by Milestone'),
+		array('priv'=>'',	'opt'=>'',				'link'=>'tasksremoved',		'name'=>'Cancelled Tasks')
+	);
+	
+	$taskrptmenu = array(
+		array('priv'=>'',	'opt'=>'',				'link'=>'taskcompleted',	'name'=>'Completed'),
+		array('priv'=>'',	'opt'=>'',				'link'=>'taskrecentdone',	'name'=>'Recently Completed '),
+		array('priv'=>'',	'opt'=>'ApproveTasks',	'link'=>'taskapproved',		'name'=>'Approved'),
+		array('priv'=>'',	'opt'=>'ApproveTasks',	'link'=>'taskrecentappr',	'name'=>'Recently Approved '),
+		array('priv'=>'',	'opt'=>'ReleaseTasks',	'link'=>'releasetasks',		'name'=>'Release Tasks'),
+		array('priv'=>'',	'opt'=>'ReleaseTasks',	'link'=>'taskrecentrel',	'name'=>'Recently Released ')
+	);
+	
+	$projectmenu = array(
+		array('priv'=>'',	'opt'=>'',				'link'=>'projects',			'name'=>'Projects'),
+		array('priv'=>'',	'opt'=>'',				'link'=>'projectareas',		'name'=>'Project Areas'),
+		array('priv'=>'',	'opt'=>'Milestones',	'link'=>'milestones',		'name'=>'Milestones'),
+		array('priv'=>'S',	'opt'=>'',				'link'=>'projectdefaults',	'name'=>'Project Defaults'),
+		array('priv'=>'S',	'opt'=>'',				'link'=>'projectadd',		'name'=>'Add Project')
+	);
+	
+	$supermenu = array(
+		array('priv'=>'G',	'opt'=>'Organizations',	'link'=>'companies',		'name'=>'Organizations'),
+		array('priv'=>'U',	'opt'=>'',				'link'=>'users',			'name'=>'Users'),
+		array('priv'=>'U',	'opt'=>'',				'link'=>'useradd',			'name'=>'Create User'),
+		array('priv'=>'G',	'opt'=>'Organizations',	'link'=>'orgtypes ',		'name'=>'Organizaton Types'),
+		array('priv'=>'G',	'opt'=>'',				'link'=>'userstatuses ',	'name'=>'User Status'),
+		array('priv'=>'G',	'opt'=>'',				'link'=>'taskstatuses ',	'name'=>'Task Status'),
+		array('priv'=>'G',	'opt'=>'',				'link'=>'purgetime ',		'name'=>'Purge Time'),
+		array('priv'=>'G',	'opt'=>'',				'link'=>'purgetask ',		'name'=>'Purge Task')
+	/*
+	,
+		array('priv'=>'G',	'link'=>'settings',			'name'=>'Settings')
+	*/
+	);
 
-	echo "<li><a id='".$menuid."' class='link'>$menuname  <div id='' class='arrow-left'></div></a></li>";
+	echo "<li><a id='".$menuid."' class='link'>$menuname  <div id='' class='arrow-left'></div></a>";		
+		
+	echo "</li>";
 	
 }
-function ShowSubMenu($alp, $menuname, $timemenu, $menuid)
+	/*
+	if ($alp->UserSetting('Time') && $db->IsOwnerOrg())
+	ShowSubMenu($alp, $menuname, $timemenu, 'Time');
+	
+	if ($db->IsGlobalSupervisor()) {
+	if ($alp->UserSetting('Time') || $alp->UserSetting('TaskCost'))
+		ShowSubMenu($alp, $menuname, $timerptmenu, 'Financial');
+	}
+	
+	ShowSubMenu($alp, 'Tasks', $taskmenu, 'Tasks');
+	ShowSubMenu($alp, $menuname, $taskrptmenu, 'CompleteTask');
+	
+	if ($db->IsProjectSupervisor())
+	ShowSubMenu($alp, $menuname, $projectmenu, 'Projects');
+	
+	if ($db->IsGlobalSupervisor() || $db->IsUserManager())
+	ShowSubMenu($alp, $menuname, $supermenu, 'Supervisor');		
+*/		
+function ShowSubMenu($alp, $menuname, $menuitems, $menuid)
 {
 	$db = $alp->Model();
-	
+	echo "<div class='sub-menu'>";
 	echo "<ul class='child-menu' style='display:none' id='SubMenu_".$menuid."'>";
 
-	foreach ($timemenu as $menu) {
+	foreach ($menuitems as $menu) {
 		if ($menu['priv'] == 'G')
 			$ok = $db->IsGlobalSupervisor();
 		else if ($menu['priv'] == 'S')
@@ -90,6 +112,7 @@ function ShowSubMenu($alp, $menuname, $timemenu, $menuid)
 			echo '<li><a href="'.$menu['link'].'">'.$menu['name'].'</a></li>';
 	}
 	echo '</ul>';
+	echo "</div>";
 }
 ?>
 <div class="menu">
@@ -117,7 +140,7 @@ if ($db->IsGlobalSupervisor() || $db->IsUserManager())
 	</ul>
     <br clear="all" />
     
-    <div class="sub-menu">
+<?php /*?>    <div class="sub-menu">
 <?php
 if ($this->UserSetting('Time') && $db->IsOwnerOrg())
 	ShowSubMenu($this, 'Time', $timemenu, 'Time');
@@ -136,5 +159,15 @@ if ($db->IsProjectSupervisor())
 if ($db->IsGlobalSupervisor() || $db->IsUserManager())
 	ShowSubMenu($this, 'Supervisor', $supermenu, 'Supervisor');
 ?>
-    </div>
+    </div><?php */?>
+
+</div>
+<div class="StaticSubMenu">
+    <ul class="static-child-menu" id="SubMenu_Time">
+        <li><a href="signin">Project Sign In</a></li>
+        <li><a href="signout">Signout</a></li>
+        <li><a href="recordtime">Record Time</a></li>
+        <li><a href="mytime">My Time</a></li>
+        <li><a href="signedin">Signed-in Staff</a></li>
+    </ul>
 </div>
